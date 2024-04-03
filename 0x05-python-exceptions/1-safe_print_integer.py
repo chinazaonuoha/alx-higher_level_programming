ff74ea058ba7edaee889eed2949d0784b9f41015
chinazaonuoha <developerworkpace@gmail.com>
2024-04-03 18:05:30 +0100

    prints an integer with {:d}.format(). fix 0x01

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,15 +8,7 @@ safe_print_integer - print only integer
 
 def safe_print_integer(value):
     try:
-        # Convert value to integer
-        integer_value = int(value)
-        if str(integer_value) == str(value):
-            print("{:d}".format(integer_value))
-            return True
-        else:
-         print(value)
-         return True
+        print("{:d}".format(int(value)))
+        return True
     except ValueError:
-        print(False)
         return False
-                    
